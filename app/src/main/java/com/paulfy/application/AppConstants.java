2089d0cc568c105eaeb679e8d339a1bdc7558a8b
Bhavesh <bsoni350@gmail.com>
2018-04-26 14:09:57 +0530

    commit by bhavesh

--- app/src/main/java/com/paulfy/application/AppConstants.java
+++ app/src/main/java/com/paulfy/application/AppConstants.java
@@ -8,4 +8,6 @@ public class AppConstants {
     public static final String DEVICE_TOKEN = "deviceToken";
     public static final String BASE_URL = "http://phpstack-151385-492769.cloudwaysapps.com/api/";
     public static final String IS_LOGIN = "isLogin";
+
+
 }
